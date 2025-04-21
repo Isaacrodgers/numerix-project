f7b804550ea7af1a1355bc37096e2fdfcb2c15bb
Isaacrodgers <162332330+Isaacrodgers@users.noreply.github.com>
2025-04-21 10:02:04 +0300

    Update calc.c marema

--- calc.c
+++ calc.c
@@ -9,7 +9,7 @@ int main() {
     // Display the menu of available operators
     printf("Enter an operator (+, -, *, /, ^, r, s, c, t, h):\n");
     printf("  + : Addition\n");
-    printf("  - : Subtractio
+    printf("  - : Subtraction\n");
     printf("  * : Multiplication\n");
     printf("  / : Division\n");
     printf("  ^ : Power\n");
